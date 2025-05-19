8e5790c3c927623f8e7c1412bc21c055c9324af2
blindelectron <82350043+blindelectron@users.noreply.github.com>
2025-05-19 09:28:01 -0500

    Removes the use of ifconfig.me from the weather js script as it is not nessesary and was just slowing things down.

--- static/js/weather.js
+++ static/js/weather.js
@@ -1,14 +1,5 @@
-// weather.js
-
 async function fetchWeatherData() {
-    try {
-        // Step 1: Fetch user's IP address
-        const ipResponse = await fetch('https://ifconfig.me/ip');
-        if (!ipResponse.ok) throw new Error('Failed to fetch IP address');
-        const ip = await ipResponse.text();
-
-        // Step 2: Fetch weather data
-        const weatherResponse = await fetch(`https://wttr.in/${ip}?format=j1`);
+    try {        const weatherResponse = await fetch(`https://wttr.in/?format=j1`);
         if (!weatherResponse.ok) throw new Error('Failed to fetch weather data');
         const weatherData = await weatherResponse.json();
 
@@ -22,7 +13,7 @@ async function fetchWeatherData() {
         const moonPhase = weatherData.weather[0].astronomy[0].moon_phase;
 
         // Return the data for use in templates
-        return { weatherDescription, tempC, tempF, city, state, country, moonPhase, ipResponse};
+        return { weatherDescription, tempC, tempF, city, state, country, moonPhase};
     } catch (error) {
         console.error('Error fetching weather data:', error);
         return null; // Return null if there's an error
